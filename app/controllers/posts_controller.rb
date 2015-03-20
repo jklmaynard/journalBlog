0fe05595b7d53060fb05f4f40f9035325fc578ef
James Maynard <jameskirkmaynard@gmail.com>
2015-03-20 10:39:50 -0700

    Troubleshoot through create_post method.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,15 +4,21 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = Post.new(params[:post])
   end
 
   def create
-    @post = Post.new(params[:post])
-    if @list.save
+    @post = Post.new( post_params )
+    if @post.save
       redirect_to posts_path
     else
       render :new
     end
+  end
+
+
+private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
